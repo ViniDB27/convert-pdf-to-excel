56f0ae93534449d9e3058fef68b8ff6ddb82a1b9
vinicios deivid brejinski <viniciosdb@gmail.com>
2024-03-07 20:48:21 -0300

    le do terminal a pasta

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 import tabula
 
-pasta = '/media/vinicio/arquivos de prog/projetos/cursos/convertPDFtoXLSX/pdfs'
-
+#pasta = '/media/vinicio/programas/projetos/estudos/convert-pdf-to-excel/pdfs'
+pasta = input('Digite o caminha da pasta onde estão os PDF`s: ')
 
 caminhos = [os.path.join(pasta, nome) for nome in os.listdir(pasta)]
 arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
@@ -15,7 +15,7 @@ for arquivo in pdfs:
     tabula.convert_into(f'{arquivo}', f'{arquivo.replace(".pdf", ".csv")}', output_format='csv', pages='all')
     print('Convertido para => ', arquivo.replace(".pdf", ".csv"))
 
-
+print('Fim do programa!')
 
 
 
